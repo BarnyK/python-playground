d4a9fba9e3ffc76efb39be85238af5d673262667
Barny <BarnyK@users.noreply.github.com>
2019-03-14 00:20:48 +0100

    Update war.py
    Translated also some quality of code changes

--- war.py
+++ war.py
@@ -10,11 +10,14 @@ from random import shuffle
 import time
 
 C_VALS = {'A':14,'K':13,'Q':12,'J':11,'1':10}
+C_SUITS_DICT = {"D":"Diamonds", "H":"Hearts", "C":"Clubs", "S":"Spades"}
 C_SUITS = ['D','H','C','S']
 C_FACES = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
 class card():
-    #klasa karty, posiada informacje o nazwie i wartości karty
+    """
+    Card class contains values of suit and a face of a card
+    """
     def __init__(self,name):
         self.n = name
         if name[0] in '1JQKA':
@@ -23,51 +26,53 @@ class card():
             self.val = int(name[0])
 
     def value(self):
-        #oddaje wartość karty
+        # Returns value of a card
         return self.val
 
     def name(self):
-        #oddaje nazwe karty
+        # Returns a name of a card
         return self.n
+
+    def __repr__(self):
+        return self.n
+
+    def __str__(self):
+        return "{} of {}".format(self.val,C_SUITS_DICT[self.n[-1]]) 
 
 
 class deck():
     def __init__(self, empty=False):
-        #tworzy talie kart, przyjmuje True/False czy ma być pusta czy nie
+        # Creates a deck of cards, empty or full 
         self.cards = []
         if(empty==False):
             self.cards = [card(x+y) for y in C_SUITS for x in C_FACES]
 
     def __repr__(self):
-        #oddaje liste kart jako string
-        return ', '.join([c.name() for c in self.cards])
+        return ', '.join([c.__repr__() for c in self.cards])
 
-    def lists(self):
-        #oddaje liste kart w talii jako liste
-        return self.cards
 
     def shuffles(self):
-        #tasuje karty
+        # Shuffles the deck of card
         shuffle(self.cards)
 
     def give(self):
-        #oddaje karte z wierzchu
+        # Returns a card from the front
         try:
             return self.cards.pop(0)
         except:
             print('pusto')
 
     def add(self, *elems):
-        #dodaje karte na spod
+        # Adds a card to the bottom
         for elem in elems:
             self.cards.append(elem)
 
     def count(self):
-        #oddaje ilość kart
+        # Returns a count of cards
         return len(self.cards)
 
     def isEmpty(self):
-        #sprawdza czy pusta
+        # Returns true if no cards 
         if self.cards:
             return False
         else:
@@ -75,24 +80,25 @@ class deck():
 
 
 def fight(card_a,card_b):
-    #funkcja porównująca karty i wywołująca wojnę
-    #temu kto wygrał daje karty ze stołu
+    # Function compares cards and initiates a war
+    # Whoever wins gets cards from table
     if card_a.value()>card_b.value():
-        for c in t_a.lists():
+        for c in t_a.cards:
             a.add(t_a.give())
-        for c in t_b.lists():
+        for c in t_b.cards:
             a.add(t_b.give())
         return True
+    
     elif card_b.value()>card_a.value():
-        for c in t_a.lists():
+        for c in t_a.cards:
             b.add(t_a.give())
-        for c in t_b.lists():
+        for c in t_b.cards:
             b.add(t_b.give())
         return True
     else:
-        #wywołuje wojne i sprawdza czy odda True czy False
-        #false oznacza, że któryś z graczy nie ma kart do 'wojny'
-        #więc oddaje False by skończy gre i ogłosić remis
+        # Initiates war and checks if it return true or false
+        # If a player doesn't have enough cards for war
+        # it returns a code for draw
         if war():
             return True
         else:
@@ -100,8 +106,8 @@ def fight(card_a,card_b):
 
 
 def war():
-    #sprwdza czy gracze mają karty potrzebne do zagrania 'wojny'
-    #jeśli mają to wykonuje akcje 'wojny', oddaje False jeśli nie mają
+    # Checks if players have enough cards for war
+    # If they have it initates war actions
     if a.count()>4 and b.count()>4:
         for x in range(4):
             ca = a.give()
@@ -115,45 +121,49 @@ def war():
     else:
         return False
     
-#tworzenie i tasowanie kart
+# Creation of main deck of cards
 cards = deck()
 cards.shuffles()
 
-#tworzenie pustych talii dla graczy oraz dla kart wyłożonych na stół
+# Creation of player decks and table stacks
 a = deck(empty=True)
 b = deck(empty=True)
 t_a = deck(empty=True)
 t_b = deck(empty=True)
 
-#rozdawanie kart
+# Dealing the cards
 for x in range(26):
     a.add(cards.give())
     b.add(cards.give())
 
+
 print('Karty pierwszego: {}'.format(a))
 print('Karty drugiego: {}'.format(b))
 
-#rozpoczęcie się pętli z grą, na początku sprawdzane jest czy ktoś nie wygrał w poprzedniej turze
-#jeśli tak:  wypisuje kto i przerywa loop oraz wypisuje ilość tur
-#jeśli nie: dodaje +1 do ilości tur i wywołuje akcje do gry
+# Start of game loop, first checks if the games wasn't won already
+# If it was, says who won and how many turns it took
+# If it wasn't, the game still loops
 
-tury=0
+turns=0
 while True:
-    if tury>5000:
+    if turns>5000:
         print('Turn overflow')
         break
     if a.isEmpty():
-        print('Drugi wygrywa')
+        print('Second player won')
         break
     if b.isEmpty():
-        print('Pierwszy wygrywa')
+        print('First player won')
         break
-    tury+=1
+    turns+=1
     c_a = a.give()
     t_a.add(c_a)
+    
     c_b = b.give()
     t_b.add(c_b)
+    
     if not fight(c_a,c_b):
-        print('Remis')
+        print('Draw')
         break
-print(tury)
+    
+print(turns)
